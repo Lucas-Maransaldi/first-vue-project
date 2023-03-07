199964969426e69b0b8b423046f60e5e7004fc68
Lucas Maransaldi <Lucas_maransaldi@hotmail.com>
2023-03-07 12:11:06 -0300

    feat: make first vue project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline " :class="{ 'modo-escuro': isDarkMode}">
+    <div class="column is-one-quarter">
+      <BarraLateral @toggle-theme="_changeTheme"/>
+    </div>
+    <div class="column is-three-quarter conteudo">
+      <FormularioPrincipal @tarefa-finalizada="salvarTarefa"/>
+      <div class="lista">
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+        <Box v-if="isTarefasEmpty()">
+          Você não esta muito produtivo hoje :(
+        </Box>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Box from './components/Box.vue';
+import FormularioPrincipal from './components/FormularioPrincipal.vue';
+import Tarefa from './components/Tarefa.vue';
+import ITarefa from  './interfaces/ITarefa'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BarraLateral,
+    FormularioPrincipal,
+    Tarefa,
+    Box
+},
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      isDarkMode: false
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa): void {
+      this.tarefas.push(tarefa);
+    },
+    isTarefasEmpty(): boolean {
+      return this.tarefas.length === 0;
+    },
+    _changeTheme(isDarkMode: boolean): void {
+      this.isDarkMode = isDarkMode;
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .lista {
+    padding: 1.25rem;
+  }
+  main {
+    --bg-primario: #fff;
+    --texto-primario: #000;
+  }
+  main.modo-escuro {
+    --bg-primario: #2b2b2b;
+    --texto-primario: #ddd;
+  }
+  .conteudo {
+    background: var(--bg-primario);
+  }
 </style>
